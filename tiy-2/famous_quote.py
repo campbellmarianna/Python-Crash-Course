15ee2891f7b419376c8e31a9691cb64f812da32e
Marianna <mckd2018@gmail.com>
2019-05-27 15:48:58 -0700

    add try it yourself solutions to  exercises

---
+++ tiy-2/famous_quote.py
@@ -0,0 +1,2 @@
+quote = 'Steve Jobs once said, "Those who are crazy enough to think they can change the world usually do."'
+print(quote)
---
+++ tiy-2/name_cases.py
@@ -0,0 +1,5 @@
+name = 'nancy wilson'
+
+print("Lowercase: {}".format(name.lower()))
+print("Uppercase: {}".format(name.upper()))
+print("Titlecase: {}".format(name.title()))
