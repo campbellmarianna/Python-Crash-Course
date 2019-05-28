db0a74fb3558e4fb962a7a9478673d9fed8c6a65
Marianna <mckd2018@gmail.com>
2019-05-28 16:32:39 -0700

    finishined try it yourself exercises for section 2

---
+++ tiy-2/famous_quote_2.py
@@ -0,0 +1,4 @@
+famous_person = 'Maya Angelou'
+message = '"I\'ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."'
+quote = famous_person + ' once said,' + message
+print(quote)
---
+++ tiy-2/stripping_names.py
@@ -0,0 +1,5 @@
+name = '\n Nancy\n\tMary\n\t\tWilson  '
+print(name)
+print(f'***No Whitespace Left: \n{name.lstrip()}')
+print(f'***No Whitespace Right: \n{name.rstrip()}')
+print(f'***No Whitespace Around: \n{name.strip()}')
