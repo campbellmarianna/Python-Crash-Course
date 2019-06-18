81cb9ba7a999e0ea8bfa9fbddf1f4f8afccb3fe3
Marianna <mckd2018@gmail.com>
2019-06-18 14:42:36 -0700

    [add] implementing list operations - in progress for lists completing Lists chapter

---
+++ list-3/motorcycles.py
@@ -0,0 +1,41 @@
+# Adding
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+#
+# motorcycles.append('ducati')
+# print(motorcycles)
+
+# motorcycles = []
+#
+# motorcycles.append('honda')
+# motorcycles.append('yamaha')
+# motorcycles.append('suzuki')
+#
+# print(motorcycles)
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+#
+# motorcycles.insert(0, 'ducati')
+# print(motorcycles)
+
+# Removing
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# del motorcycles[1]
+# print(motorcycles)
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# popped_motorcycle = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycle)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a " + first_owned.title() + ".")
